a0e461133f7b458aa71a3ad8f937ea15db76b25c
wanguinuthu <wanguinuthu5@gmail.com>
2022-11-07 14:38:10 +0300

    setup production build

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const express =require('express')
 const app = express()
-const publicPath = path.join(_dirName,'..','public')
-const path = process.nextTick.PORT || 3000
+const publicPath = path.join(__dirname,'..','public')
+const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath))
 
